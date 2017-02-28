fe1236f2a867aa859e0152a7a819b69fae0b0532
gdshen <gdshen95@gmail.com>
2017-02-28 19:55:49 +0800

    change default size and offset for multiple draw

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 # pyenv shell 3.6.0
 
 # generate train test split
-python train_test_split.py --chars_file characters/top_3000_simplified.txt --output_dir characters
+# python train_test_split.py --chars_file characters/top_3000_simplified.txt --output_dir characters
 
 # generate train images
 python draw_multiple_characters.py --font font/simkai.ttf --ch_lists characters/train.txt --gray --output_dir output/top_3000/train/simkai
